70ca2d18d5410519745876cee4e12294877f571a
ifysglbiydgfifs <igbsh43hsdh@gmail.com>
2025-06-16 04:32:45 +0500

    lab 10

--- src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
+++ src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
@@ -1,44 +1,41 @@
 package com.example.messagingrabbitmq;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class MessagingRabbitmqApplication {
 
-	static final String topicExchangeName = "spring-boot-exchange";
-
-	static final String queueName = "spring-boot";
+	static final String fanoutExchangeName = "spring-boot-fanout-exchange";
 
 	@Bean
-	Queue queue() {
-		return new Queue(queueName, false);
+	FanoutExchange fanoutExchange() {
+		return new FanoutExchange(fanoutExchangeName);
 	}
 
 	@Bean
-	TopicExchange exchange() {
-		return new TopicExchange(topicExchangeName);
+	Queue anonymousQueue() {
+		return new AnonymousQueue();
 	}
 
 	@Bean
-	Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	Binding binding(Queue anonymousQueue, FanoutExchange fanoutExchange) {
+		return BindingBuilder.bind(anonymousQueue).to(fanoutExchange);
 	}
 
 	@Bean
-	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-			MessageListenerAdapter listenerAdapter) {
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter, Queue anonymousQueue) {
 		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
 		container.setConnectionFactory(connectionFactory);
-		container.setQueueNames(queueName);
+		container.setQueues(anonymousQueue);
 		container.setMessageListener(listenerAdapter);
 		return container;
 	}
@@ -48,8 +45,7 @@ public class MessagingRabbitmqApplication {
 		return new MessageListenerAdapter(receiver, "receiveMessage");
 	}
 
-	public static void main(String[] args) throws InterruptedException {
-		SpringApplication.run(MessagingRabbitmqApplication.class, args).close();
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingRabbitmqApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/messagingrabbitmq/Runner.java
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -1,27 +1,34 @@
 package com.example.messagingrabbitmq;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
 
 @Component
 public class Runner implements CommandLineRunner {
 
 	private final RabbitTemplate rabbitTemplate;
-	private final Receiver receiver;
 
-	public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
-		this.receiver = receiver;
+	public Runner(RabbitTemplate rabbitTemplate) {
 		this.rabbitTemplate = rabbitTemplate;
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Sending message...");
-		rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
-	}
+	public void run(String... args) {
+		System.out.println("Type smth...");
 
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.print("> ");
+			String input = scanner.nextLine();
+			if (input == null || input.trim().isEmpty()) {
+				System.out.println("Shutdown...");
+				break;
+			}
+
+			rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.fanoutExchangeName, "", input);
+		}
+	}
 }
